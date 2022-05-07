61ab4d3e21960b23be8fb7bd2ea7de6ee081b3c7
Akashpradhan-Git <akashpradhan151@gmail.com>
2022-05-07 17:57:59 +0530

    api

--- services/api.js
+++ services/api.js
@@ -3,7 +3,7 @@ let API = "";
 if (process.env.NODE_ENV === "development") {
     API = "http://localhost:8050/e-commerce/api";
 } else if (process.env.NODE_ENV === "production") {
-    API = "http://localhost:8050/e-commerce/api";
+    API = "http://192.168.3.100:8050/e-commerce/api";
 }
 
 export const API_HOST = API;
